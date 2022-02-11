430d27c652ffa189a74c3dbbfe56bdb4d0cb7a3d
anisha-s <77512542+anisha-s@users.noreply.github.com>
2022-02-10 23:42:23 -0700

    web client

--- httpclient.py
+++ httpclient.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # coding: utf-8
-# Copyright 2016 Abram Hindle, https://github.com/tywtyw2002, and https://github.com/treedust
+# Copyright 2022 Abram Hindle, https://github.com/tywtyw2002, https://github.com/treedust and Anisha Sethumadhavan
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,7 +13,12 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+#
+# References:
+# bgporter( 9 April 2011) and mb21(edited, 15 November 2018), CC BY-SA 4.0, https://stackoverflow.com/a/5607708
+# alvas, 3 January 2014, CC BY-SA 3.0, https://stackoverflow.com/a/20901803
+# Bigyan Karki, CMPUT 404 LAB 2, 17 January 2022, https://drive.google.com/file/d/1TD8UR9o-GPaudPNsY9HrePzroRBsEBFe/view
+#
 # Do not use urllib's HTTP GET and POST mechanisms.
 # Write your own HTTP GET and POST
 # The point is to understand what you have to send and get experience with it
@@ -41,13 +46,21 @@ class HTTPClient(object):
         return None
 
     def get_code(self, data):
-        return None
+        status_code = data.split()[1]
+        #print("get_code data split  ", data.split())
+        #print("get_code sc   ", status_code)
+        return int(status_code)
 
-    def get_headers(self,data):
-        return None
+    def get_headers(self, data):
+        # headers end with "\r\n\r\n", so we split them
+        #print("get_h split  ", data.split("\r\n\r\n"))
+        headers = data.split("\r\n\r\n")[0]
+        return headers
 
     def get_body(self, data):
-        return None
+        # body is below headers so we need to take data after "\r\n\r\n"
+        body = data.split("\r\n\r\n")[1]
+        return body
     
     def sendall(self, data):
         self.socket.sendall(data.encode('utf-8'))
@@ -67,14 +80,70 @@ class HTTPClient(object):
                 done = not part
         return buffer.decode('utf-8')
 
+    """
+    URlparser is a function that parses through the url
+    :param url to be parsed
+    :return port, path and host from the parsed url
+    """
+    def url_parser(self, url):
+        # parse the url
+        parsed_url = urllib.parse.urlparse(url)
+
+        # get the port
+        port = parsed_url.port
+        if not parsed_url.port:
+            port = 80
+
+        # get the path
+        path = parsed_url.path
+        if parsed_url.path == "":
+            path = "/"
+
+        # get the host
+        host = parsed_url.hostname
+        return port, path, host
+
     def GET(self, url, args=None):
-        code = 500
-        body = ""
+        #code = 500
+        #body = ""
+        port, path, host = self.url_parser(url)
+        get_request_header = f'GET {path} HTTP/1.1\r\nHost: {host}\r\nAccept: */*\r\nConnection: Closed\r\n\r\n'
+        self.connect(host, port)
+        self.sendall(get_request_header)
+        data = self.recvall(self.socket)
+        code = self.get_code(data)
+        body = self.get_body(data)
+        self.close()
+        print(body)
         return HTTPResponse(code, body)
 
     def POST(self, url, args=None):
-        code = 500
-        body = ""
+        #code = 500
+        #body = ""
+        port, path, host = self.url_parser(url)
+        #print("PoSt XXXXXXXXXXXXXXXXXXXX  ", port, path, host)
+        self.connect(host, port)
+        #print("connected")
+
+        if args == None:
+            content_length = str(0)
+            payload = ""
+        else:
+            payload = (urllib.parse.urlencode(args))
+            #print("payload  ", payload)
+            content_length = str(len(payload))
+
+        post_request_header = f'POST {path} HTTP/1.1\r\nHost: {host}\r\nAccept: */*\r\nConnection: Closed\r\nContent-Length: {content_length}\r\nContent-Type: application/x-www-form-urlencoded\r\n\r\n{payload}\r\n'
+
+        self.sendall(post_request_header)
+        #print("sendall")
+        data = self.recvall(self.socket)
+        #print("data   ", data)
+        code = self.get_code(data)
+        #print("code   ", code)
+        body = self.get_body(data)
+        self.close()
+        # print(body)
         return HTTPResponse(code, body)
 
     def command(self, url, command="GET", args=None):
